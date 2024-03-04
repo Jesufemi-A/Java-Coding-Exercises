cdd0030f0a58ba8b691cff3231bf1de6023318eb
Jesufemi-A <jesufemia1@gmail.com>
2024-03-04 23:59:27 +0100

    sum of odd and even in array

--- Coding.java
+++ Coding.java
@@ -18,8 +18,33 @@ public class Coding {
         {}	                            0 (no middle element)
         {10}	                        1 (the middle element 10 is strictly less than all other elements) */
 
-        int [] arr =      {3, 2, 1, 4, 1}	 	 	;
-        System.out.print(centeredArray(arr));
+        int [] arr =      {3, 2, 1, 4, 1};
+        System.out.println(centeredArray(arr));
+
+        /* Q2
+
+        Write a function that takes an array of integers as an argument and returns a value based on the sums of the even and odd numbers in the array.
+        Let X = the sum of the odd numbers in the array and let Y = the sum of the even numbers. The function should return X – Y
+        The signature of the function is:
+        int f(int[ ] a)
+
+        Examples
+
+        if input array is	            return
+        {1}	                            1
+        {1, 2}	                        -1
+        {1, 2, 3}	                    2
+        {1, 2, 3, 4}	                -2
+        {3, 3, 4, 4}	                -2
+        {3, 2, 3, 4}	                 0
+        {4, 1, 2, 3}	                -2
+        {1, 1}	                          2
+        {}	                              0
+         */
+
+
+        int [] arr1 =         {}	 	;
+        System.out.println(f(arr1));
 
     }
 
@@ -38,6 +63,31 @@ public class Coding {
         }
 
         return 1;
+    }
+
+
+    // Q2 solution
+
+    public  static int f(int[] a) {
+        if (a.length == 0 || a == null)
+            return 0;
+        else  if (a.length == 1)
+            return a[0];
+
+        // if length > 1
+
+        int odd = 0;
+        int even =  0;
+        for (int i = 0;  i < a.length; i++) {
+            if (a[i] % 2 == 0)
+                even += a[i];
+            else
+                odd += a[i];
+
+
+        }
+
+        return odd - even;
     }
 
 }
